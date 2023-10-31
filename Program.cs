0550a2dbb78bd95733d6b15a1c1dc53841734df0
unknown <eminra@code.edu.az>
2023-10-31 15:11:14 +0400

    firts

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿class Area
+{
+    public static void Main()
+    {
+
+        Console.WriteLine (PrintSahe(3));
+        Console.WriteLine (PrintDuzbsahe(3,5));
+        Console.WriteLine(PrintSahe(3));
+
+
+    }
+
+    static int PrintSahe(int number)
+    {
+
+        return number * 3;
+        static int PrintSahe(int number)
+        {
+
+            return number * 3;
+
+        }
+
+        static int PrintDuzbsahe(int number1, int number2)
+        {
+
+            return number1 * number2;
+
+            
+
+            static int PrintDaxilicevre(int a, int b, int c, int r)
+            {
+
+                return (a + b + c) / 2 * r;
+            }
+        
---
+++ funksiyacem.cs
@@ -0,0 +1,14 @@
+﻿int getsum (int target)
+{
+    int sum = 0;
+
+
+    int number = 0;
+    while(sum +number < target)
+    {
+        sum=sum+number;
+        number++;
+        
+    }return sum;
+}
+Console.WriteLine(getsum(11));
