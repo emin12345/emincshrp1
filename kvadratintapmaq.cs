548c718496131f9cd229bf67e1df76d4e1371075
unknown <eminra@code.edu.az>
2023-10-31 15:12:59 +0400

    task

---
+++ kvadratintapmaq.cs
@@ -0,0 +1,16 @@
+﻿
+int result;
+
+
+
+while (true)
+{
+    Console.WriteLine("Eded daxil edin");
+    int number = int.Parse(Console.ReadLine());
+    result = number * number;
+Console.WriteLine(result);
+}
+
+
+
+
